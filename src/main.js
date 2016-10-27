cabf5a3566f29f235c60ba012a2202ca0ad1f4bb
rangel <18792561236@163.com>
2016-10-27 22:24:39 +0800

    modifu reducer

--- src/main.js
+++ src/main.js
@@ -5,24 +5,26 @@ import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers/index.js';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import LoginApp from './containers/login/login-container';
+// import LoginApp from './containers/login/login-container';
 import createLogger from 'redux-logger';
 import PaperList from './containers/paper-list/paper-list';
 import Teacher from './components/teacher/Teacher';
+import PaperMeta from './containers/paper-meta/paper-meta';
 
 const store = createStore(
-  rootReducer,
-  applyMiddleware(createLogger(), thunkMiddleware)
+    rootReducer,
+    applyMiddleware(createLogger(), thunkMiddleware)
 );
 
 render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path={URI_PROFIX} component={LoginApp}/>
-      <Route path={URI_PROFIX + '/paperList'} components={PaperList}/>
-      <Route path={URI_PROFIX + '/teacher'} components={Teacher}/>
-    </Router>
-  </Provider>,
-  document.getElementById('app')
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path={URI_PROFIX} component={PaperMeta}/>
+            <Route path={URI_PROFIX + '/paperList'} components={PaperList}/>
+            <Route path={URI_PROFIX + '/teacher'} components={Teacher}/>
+            {/*<Route path={URI_PROFIX + '/paperMeta'} components={PaperMeta}/>*/}
+        </Router>
+    </Provider>,
+    document.getElementById('app')
 );
 
--- src/reducers/paper-meta/paper-meta.js
+++ src/reducers/paper-meta/paper-meta.js
@@ -1,10 +1,13 @@
 export default (state = false, action) => {
+    console.log(action.result);
     switch (action.type) {
         case 'PAPER_META': {
-            if (action.result.url) {
+            if (action.result) {
                 alert("修改成功");
+            }else {
+                alert("修改失败");
             }
         }
     }
-    alert("修改失败");
+    return state;
 };
