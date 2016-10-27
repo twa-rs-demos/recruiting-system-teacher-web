c11e2c9e33067610d750e2fd0b86c7c492c92393
MHwishes <1025383143@qq.com>
2016-10-27 21:12:49 +0800

    resolve conflict

--- src/reducers/paper-meta/paper-meta.js
+++ src/reducers/paper-meta/paper-meta.js
@@ -1,10 +1,14 @@
-export default (state = false, action) => {
+export default(state =false, action)=> {
+    console.log(action);
     switch (action.type) {
         case 'PAPER_META': {
-            if (action.result.url) {
-                alert("修改成功");
+            if (action.result) {
+                 alert("修改成功");
+            }else {
+                alert("修改失败");
             }
         }
     }
-    alert("修改失败");
-};
+    return state;
+}
+
