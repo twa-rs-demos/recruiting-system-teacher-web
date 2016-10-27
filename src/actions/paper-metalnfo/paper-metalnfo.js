ae3ed79d569684222ab99da02280dd20e120b895
MHwishes <1025383143@qq.com>
2016-10-27 13:22:19 +0800

    update paper meta action

--- src/actions/paper-metalnfo/paper-metalnfo.js
+++ src/actions/paper-metalnfo/paper-metalnfo.js
@@ -1,3 +1,13 @@
 export  const  modifyMetaInfo=(title,description,easyCount,normalCount,hardCount)=>{
-
-}
+    return (dispatch)=> {
+        superagent
+            .get('/api/papers-metaInfo')
+            .end((err, res) => {
+                if (err) {
+                    throw(err);
+                } else {
+                    dispatch(getMeta(res.body));
+                }
+            });
+    }
+};
